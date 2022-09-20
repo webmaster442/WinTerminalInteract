8459971d57c254bd2e999c9af5e7e64ceae5b201
webmaster442 <ruzsinszki.gabor@gmail.com>
2022-09-20 20:15:09 +0200

    Terminal fragment management API

---
+++ Webmaster442.WinTerminalInteract/TerminalFragmentFile.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+
+using Webmaster442.WinTerminalInteract.Internals;
+
+namespace Webmaster442.WinTerminalInteract;
+internal static class TerminalFragmentFile
+{
+    public static void Write(string targetFile, TerminalFragment terminalFragment)
+    {
+        string json = JsonSerializer.Serialize(terminalFragment, SerializerOptionFactory.Create());
+        File.WriteAllText(targetFile, json);
+    }
+
+    public static async Task WriteAsync(string targetFile, TerminalFragment terminalFragment, CancellationToken cancellationToken = default)
+    {
+        string json = JsonSerializer.Serialize(terminalFragment, SerializerOptionFactory.Create());
+        await File.WriteAllTextAsync(targetFile, json, cancellationToken);
+    }
+
+    public static TerminalFragment Read(string sourceFile)
+    {
+        string json = File.ReadAllText(sourceFile);
+        var result = JsonSerializer.Deserialize<TerminalFragment>(json, SerializerOptionFactory.Create());
+
+        if (result == null)
+            throw new InvalidOperationException("invalid terminal fragment document");
+
+        return result;
+    }
+
+    public static async Task<TerminalFragment> ReadAsync(string sourceFile, CancellationToken cancellationToken = default)
+    {
+        string json = await File.ReadAllTextAsync(sourceFile, cancellationToken);
+        var result = JsonSerializer.Deserialize<TerminalFragment>(json, SerializerOptionFactory.Create());
+
+        if (result == null)
+            throw new InvalidOperationException("invalid terminal fragment document");
+
+        return result;
+    }
+}
